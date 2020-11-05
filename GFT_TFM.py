630e9560575e8dfcb01e391a41313c41a5bc1445
Hulxx <64197824+Hulxx@users.noreply.github.com>
2020-11-05 14:50:53 +0100

    Add files via upload

--- GFT_TFM.py
+++ GFT_TFM.py
@@ -34,7 +34,7 @@ from sklearn.svm import SVC
 
 class Model():
     def __init__(self):
-        model_dir = '/home/carlesredon/tfm/mymodel2.h5'
+        model_dir = 'D:\MDA\TFM'
         model = tf.keras.models.load_model(model_dir)
         model = model.compile()
         print('Model loaded')
